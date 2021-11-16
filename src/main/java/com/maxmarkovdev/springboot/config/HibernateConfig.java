25713cabc57efa332ec47bf2a1b791a7835deba9
Maxim-Markov <maxxmarkov@gmail.com>
2021-11-16 23:05:31 +0300

    implementation commit

--- src/main/java/com/maxmarkovdev/springboot/config/HibernateConfig.java
+++ src/main/java/com/maxmarkovdev/springboot/config/HibernateConfig.java
@@ -9,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -53,7 +51,6 @@ public class HibernateConfig {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
         Properties hibernateProperties = new Properties();
-//        hibernateProperties.put("hibernate.dialect", DIALECT);
         hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
         hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
         factoryBean.setHibernateProperties(hibernateProperties);
@@ -68,34 +65,5 @@ public class HibernateConfig {
         return transactionManager;
     }
 
-    UserService service;
-    RoleService roleService;
-
-    @Autowired
-    public void setService(UserService service, RoleService roleService) {
-        this.service = service;
-        this.roleService = roleService;
-    }
-
-    @PostConstruct
-    public void createData() {
-        Role role1 = new Role("ROLE_ADMIN");
-        Role role2 = new Role("ROLE_USER");
-        Role role3 = new Role("ROLE_UNDEFINED");
-        roleService.createRole(role1);
-        roleService.createRole(role2);
-        roleService.createRole(role3);
-
-        User user1 = new User("ADMIN", "sws", (byte) 36, "dg@xsjb.com", "admin");
-        User user2 = new User("test", "sws", (byte) 33, "dg@xsjb.com", "test");
-        User user3 = new User("tr", "sws", (byte) 39, "dg@xsjb.com", "admin");
-        user1.addRole(role1);
-        user2.addRole(role2);
-        user2.addRole(role3);
-        user3.addRole(role1);
-        service.createUser(user1);
-        service.createUser(user2);
-        service.createUser(user3);
-    }
 
 }
